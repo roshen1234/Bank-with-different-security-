5a42f84f68d3df8a5c9eda2bcdfb28e8bb3b6340
Roshen Koshy <roshankoshy123@gmail.com>
2026-09-17 20:28:20 +0530

    Learning how to enable SSO(using another datbase for login like google etc) using OAuth2.0 second method using properties only

--- springOAUTH2DemoUsingSocialLogin/src/main/java/com/example/springOAUTH2DemoUsingSocialLogin/config/ProjectSecurityConfig.java
+++ springOAUTH2DemoUsingSocialLogin/src/main/java/com/example/springOAUTH2DemoUsingSocialLogin/config/ProjectSecurityConfig.java
@@ -27,24 +27,24 @@ public class ProjectSecurityConfig {
     }
 
     //the below given step is one way to use SSO login using OAUTH 2 second method is adding in properties so no need of all these code
-    @Bean
-    ClientRegistrationRepository clientRegistrationRepository(){
-        org.springframework.security.oauth2.client.registration.ClientRegistration github = githubClientRegistration();
-        ClientRegistration facebook=facebookClientRegistration();
-
-        return new InMemoryClientRegistrationRepository(github,facebook);
-    }
-
-    private ClientRegistration githubClientRegistration()
-    {
-        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("Ov23lixMy6bfslEqKw8D")
-                .clientSecret("831747848cd15ca9518096e322efa01d8308fca2").build();
-    }
-
-    private ClientRegistration facebookClientRegistration()
-    {
-        return CommonOAuth2Provider.FACEBOOK.getBuilder("facebook").clientId("1989988658346097")
-                .clientSecret("1dc11b198c88b841313346608a65d927").build();
-    }
+//    @Bean
+//    ClientRegistrationRepository clientRegistrationRepository(){
+//        org.springframework.security.oauth2.client.registration.ClientRegistration github = githubClientRegistration();
+//        ClientRegistration facebook=facebookClientRegistration();
+//
+//        return new InMemoryClientRegistrationRepository(github,facebook);
+//    }
+//
+//    private ClientRegistration githubClientRegistration()
+//    {
+//        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("Ov23lixMy6bfslEqKw8D")
+//                .clientSecret("831747848cd15ca9518096e322efa01d8308fca2").build();
+//    }
+//
+//    private ClientRegistration facebookClientRegistration()
+//    {
+//        return CommonOAuth2Provider.FACEBOOK.getBuilder("facebook").clientId("1989988658346097")
+//                .clientSecret("1dc11b198c88b841313346608a65d927").build();
+//    }
 
 }
